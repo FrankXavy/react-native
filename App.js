eda9f12aba060ab8828d2ada87984503f821727b
Frank <ffrankxavy@hotmail.com>
2025-07-09 09:49:01 -0500

    Componentes basicos

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, Button, TouchableHighlight, Pressable } from 'react-native';
+import icon from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Image style={styles.image} source={{uri: "https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg"}} />
+      <Text>Tenemos aqui la app!</Text>
+      <Button
+        title="Click me!"
+        onPress={() => alert('Button clicked!')}
+        color={'#841584'}
+        />
+      <TouchableHighlight
+  onPress={() => alert('Touchable Highlight clicked!')}
+        style={{ backgroundColor: '#841584', padding: 10, borderRadius: 5, marginTop: 20 }}
+      
+        underlayColor="#DDDDDD">
+        <Text style={{ color: '#000' }}>Touchable Highlight</Text>
+      </TouchableHighlight>
+      <Pressable onPress={() => alert('Pressable clicked!')}>
+        <Text style={{ color: '#fff', padding: 10, backgroundColor: '#841584', borderRadius: 5, marginTop: 20 }}>Pressable</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +30,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 215,
+    height: 294,
+    borderRadius: 10,
+    marginBottom: 20,
+  }
 });
---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('eslint/config');
+const expoConfig = require("eslint-config-expo/flat");
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ["dist/*"],
+    plugins: {
+      prettier: require("eslint-plugin-prettier"),
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+    extends: [
+      "prettier"
+    ],
+  }
+]);
