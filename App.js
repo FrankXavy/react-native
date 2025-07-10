50a9002946127c1f5606759398e87d2987d412ce
frank <ffrankxavy@hotmail.com>
2025-07-10 18:44:53 -0500

    hecho en clase

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View } from 'react-native';
+import Logo from './components/Logo';
+import { Main } from './components/Main';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' />
+      <Logo style={styles.logo} />
+      <Main />
     </View>
   );
 }
@@ -17,4 +21,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    backgroundColor: '#333',
+    padding: 20
+  }
 });
---
+++ components/Main.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { getPersonajes } from "../lib/rickyMorty";
+import { CharacterCard } from "./CharacterCard";
+import { ActivityIndicator, FlatList, ScrollView } from "react-native";
+
+export function Main() {
+
+  const [personajes, setPersonajes] = useState([]);
+
+  useEffect(()=>{
+    getPersonajes().then((data)=>{
+      setPersonajes(data);
+      console.log("personajes:", data);
+    })
+  }, []);
+
+    return (
+        <>
+            {personajes.length === 0 ? (
+                <ActivityIndicator size={"large"} />
+            ): (
+                <FlatList
+                    data={personajes}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={
+                        ({item, index}) => (
+                            <CharacterCard character={item} index={index} />
+                        )
+                    }
+                >
+
+                </FlatList>
+            )
+        }
+        </>
+    );
+}
